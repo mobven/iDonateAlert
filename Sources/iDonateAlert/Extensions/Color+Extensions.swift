04aadacf91df8f3b73fe015f26ea8c8343b97866
Eser Kucuker <75912206+NOTB4D@users.noreply.github.com>
2023-02-10 20:28:20 +0300

    Add colors extention to get color

--- Sources/iDonateAlert/Extensions/Color+Extensions.swift
+++ Sources/iDonateAlert/Extensions/Color+Extensions.swift
@@ -10,8 +10,23 @@ import UIKit
 
 extension UIColor {
     
-    public static var primary: UIColor {
-        .systemPink
-    }
+    static var afadBlue: UIColor { UIColor(colorName: "afadBlue") }
+    static var afadButtonBackgroundColor: UIColor { UIColor(colorName: "afadButtonBackgroundColor") }
+    static var ahbapButtonBackgroundColor: UIColor { UIColor(colorName: "ahbapButtonBackgroundColor") }
+    static var baseDonateTextColor: UIColor { UIColor(colorName: "baseDonateTextColor") }
+    static var baseTextColor: UIColor { UIColor(colorName: "baseTextColor") }
+    static var bsaeBackgroundColor: UIColor { UIColor(colorName: "bsaeBackgroundColor") }
+    static var individualButonBorderColor: UIColor { UIColor(colorName: "individualButonBorderColor") }
+    static var kızılayBackgroundColor: UIColor { UIColor(colorName: "kızılayBackgroundColor") }
+    static var kızılayRed: UIColor { UIColor(colorName: "kızılayRed") }
     
+}
+public extension UIColor {
+    convenience init(colorName: String) {
+        if #available(iOS 13.0, *) {
+            self.init(named: colorName, in: Bundle.module, compatibleWith: .current)!
+        } else {
+            self.init(named: colorName, in: Bundle.module, compatibleWith: nil)!
+        }
+    }
 }
